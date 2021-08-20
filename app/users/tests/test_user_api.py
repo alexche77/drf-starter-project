67f90f2e202fc538603482f4faf9c3bbdcefe6a7
Alvaro <alexche7717@gmail.com>
2021-08-20 00:52:42 -0600

    Added test for user api

---
+++ app/users/tests/test_user_api.py
@@ -0,0 +1,168 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from core.tests import utils
+from users.serializers import UserSerializer
+
+USERS_URL = reverse('users-list')
+USER_DETAIL_NAME = 'users-detail'
+ME_URL = reverse('rest_user_details')
+TOKEN_URL = reverse('rest_login')
+
+
+def create(**params):
+    user = User.objects.create(**params)
+    if 'password' in params:
+        user.set_password(params['password'])
+        user.save()
+    return user
+
+
+def create_superuser(**params):
+    return User.objects.create_superuser(**params)
+
+
+class PublicUserApiTest(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_token_for_user(self):
+        """Test that a token is created for the user"""
+        payload = {
+            'username': 'testuser',
+            'email': 'testshort@test.com',
+            'password': 'password',
+        }
+        create(**payload)
+        res = self.client.post(TOKEN_URL, {
+            'username': 'testuser',
+            'password': 'password',
+        })
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('key', res.data)
+
+    def test_create_token_invalid_credentials(self):
+        """Token is not created if invalid credentials are given"""
+        payload = {
+            'username': 'testuser',
+            'email': 'test@test.com',
+            'password': 'password',
+        }
+        create(
+            email='test@test.com',
+            username='Test',
+            password='otherpassword',
+        )
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('key', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test that token is not created if user does not exists"""
+        payload = {
+            'username': 'testuser',
+            'email': 'testshort@test.com',
+            'password': 'password',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('key', res.data)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('key', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """Test that email and password are required"""
+        res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
+        self.assertNotIn('key', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authetication is required for users"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTest(TestCase):
+    """Test regular user actions"""
+
+    def setUp(self):
+        self.client = APIClient()
+        payload = {
+            'username': 'test',
+            'first_name': 'test',
+            'last_name': 'user',
+            'email': 'test@test.com',
+            'password': 'password',
+        }
+        self.user = create(**payload)
+        utils.AuthUtils.sign_in(self, self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        serializer = UserSerializer(instance=self.user)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {'first_name': 'Test', 'last_name': 'User'}
+
+        res = self.client.patch(ME_URL, payload)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, payload['first_name'])
+        self.assertEqual(self.user.last_name, payload['last_name'])
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
+class AdminUserApiTests(TestCase):
+    """Test API requests that require authentication"""
+
+    def setUp(self):
+        payload = {
+            'username': 'admin',
+            'email': 'admin@test.com',
+            'password': 'password',
+        }
+        self.admin_user = create_superuser(**payload)
+        self.client = APIClient()
+        utils.AuthUtils.sign_in(self, self.admin_user)
+        payload = {
+            'username': 'test',
+            'email': 'test@test.com',
+            'password': 'password',
+        }
+        self.user = create(**payload)
+
+    def test_list_users(self):
+        """Test creating user with valid payload is successful"""
+        res = self.client.get(USERS_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('email', res.data['data'][0])
+        self.assertEqual(self.user.email, res.data['data'][0]['email'])
+
+    def test_delete_user(self):
+        payload = {
+            'username': 'test_delete',
+            'email': 'test_delete@test.com',
+            'password': 'password',
+        }
+        user_to_delete = create(**payload)
+        user_url = reverse(
+            USER_DETAIL_NAME,
+            kwargs={'pk': user_to_delete.pk}
+        )
+        res = self.client.delete(user_url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        res = self.client.get(user_url)
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
--- ci-cd/ci.sh
+++ ci-cd/ci.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-docker-compose build
+echo "Running flake8.."
 docker-compose run app flake8
+echo "Lint passed!"
+echo "------"
+echo "Testing"
 docker-compose run app sh -c "pytest --cov app"
+echo "Finished!"
